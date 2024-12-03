ec6f99981b7c7c1804936606356368564e96f37a
Tran Quoc Khanh Nguyen <khanhnguyen.trq@gmail.com>
2024-12-02 23:37:49 -0500

    Configured update telemetry roperties

--- function_app.py
+++ function_app.py
@@ -1,6 +1,8 @@
 import azure.functions as func
 import logging
 import json
+import jsonpatch
+from datetime import datetime
 from azure.digitaltwins.core import DigitalTwinsClient
 from azure.identity import DefaultAzureCredential
 
@@ -34,11 +36,50 @@ def poc_processing_telemetry(azeventhub: func.EventHubEvent):
         if telemetry_data and twin_id:
             logging.info(f'Telemetry successfully sent to twin {twin_id}')
 
-            # Send telemetry to Azure Digital Twin
-            adt_client.publish_telemetry(
-               digital_twin_id=twin_id,  
-               telemetry=json.dumps(telemetry_data)  
-            )
+            # Update telemetry property to Azure Digital Twin
+            # patch_document =[]
+            # for key,value in telemetry_data.items():
+            #     if key != 'deviceId' and key !='time':
+            #         patch_document.append({
+            #             "op":"replace",
+            #             "path":f"/{key}",
+            #             "value":value
+            #         })
+                
+            #     if key =='time':
+            #         time_value = datetime.utcnow().isoformat() + "Z"  # Generate current timestamp in ISO format
+            #         patch_document.append({
+            #             "op": "replace",
+            #             "path": "/time",
+            #             "value": time_value
+            #         })
+
+       
+
+            # adt_client.update_digital_twin(twin_id,[
+            #     {
+            #         "op":"replace",
+            #         "path":"/temperature",
+            #         "value":30
+            #     }
+            # ])
+
+            patch_document = [
+            {
+                "op": "replace", 
+                "path": "/temperature", 
+                "value": 22.5  # Assuming temperature is a double
+            },
+            {
+                "op": "replace", 
+                "path": "/humidity", 
+                "value": 60  # Assuming humidity is a double
+            }
+            ]
+
+            adt_client.update_digital_twin(twin_id,patch_document)
+
+
             logging.info(f'Telemetry successfully sent to twin {twin_id}')
 
     except Exception as e:
